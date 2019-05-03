29881e39dc232e6e6c333d7dfc9a29e42663d7a2
nargok <nargok@gmail.com>
2019-05-03 20:55:34 +0900

    routing

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	// "encoding/json"
+	// "fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -18,5 +18,33 @@ type Book struct {
 var books []Book
 
 func main() {
-	fmt.Println("vim-go")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get all books is called")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get single book is called")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Add book is called")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("updateBook is called")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("removeBook is called")
 }
