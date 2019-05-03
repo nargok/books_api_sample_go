12d4511a83f04bdd0d3e6a4b7d2c19b0d9f5bb39
nargok <nargok@gmail.com>
2019-05-03 21:46:51 +0900

    removeBook from dummy data

--- main.go
+++ main.go
@@ -82,5 +82,14 @@ func updateBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func removeBook(w http.ResponseWriter, r *http.Request) {
-	log.Println("removeBook is called")
+	params := mux.Vars(r)
+	id, _ := strconv.Atoi(params["id"])
+
+	for i, item := range books {
+		if item.ID == id {
+			books = append(books[:i], books[i+1:]...)
+		}
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
